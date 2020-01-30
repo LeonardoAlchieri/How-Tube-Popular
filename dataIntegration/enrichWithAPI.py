41b9f6621752dacb214a505b8b4c6e293f191f1a
Leonardo Alchieri <leonardoalchieri@Leonardos-Macbook-Pro-15.local>
2020-01-30 10:52:53 +0100

    Completed enrichments API

--- dataIntegration/enrichWithAPI.py
+++ dataIntegration/enrichWithAPI.py
@@ -16,18 +16,26 @@ from apiclient.discovery import build
 from datetime import datetime
 import yaml
 
-# This one enriches scrape collections with regionCode from API
+
 
 def main():
-
     comm = MPI.COMM_WORLD
 
     ID = comm.Get_rank()
+
+    logging.basicConfig(filename='./logs/log_enrichAPI'+str(ID)+'.log', level=logging.INFO)
+    logging.info("\n")
+    logging.info("Log file created. Program started.")
+    logging.info("Reading config files.")
+
+
 
 
     with open("configMongo.yml", "r") as file:
         cfgMongo = yaml.safe_load(file)
 
+    logging.info("Config files succesfully read.")
+    logging.info("Loading Mongo collections.")
     MONGO_HOST = cfgMongo["host"]
     MONGO_DATABASE = cfgMongo["database"]
 
@@ -40,24 +48,22 @@ def main():
     collectionName = "API"
     APICollection = databaseMongo[collectionName]
 
-    collectionName = "testEnrich"
-    resultCollection = databaseMongo[collectionName]
-    print("OK0")
+    logging.info("Mongo collections loaded.")
+
     BATCH_SIZE = round(APICollection.count_documents({})/comm.Get_size() + 0.5)
     cursorAPI = APICollection.find().skip(BATCH_SIZE*ID).limit(BATCH_SIZE)
 
-    BATCH_SIZE = round(APICollection.count_documents({})/comm.Get_size() + 0.5)
-    cursorScrape = scraperCollection.find().skip(BATCH_SIZE*ID).limit(BATCH_SIZE)
-    print("OK1")
+    logging.info("Preparing to update.")
+    # This Updated enriches API documents with data from scrape
     upserts = [ UpdateOne(
         {'id':APIDoc["id"]},
-        {# If there a duplicate, the scrapign will not be done.
-            '$setOnInsert': scraper,
+        {
             '$set': {"regionCode": APIDoc["regionCode"]}
-        }, upsert=True) for APIDoc, scraper in zip(cursorAPI, cursorScrape)]
-    print("OK2")
-    resultCollection.bulk_write(upserts)
-    print("OK3")
+        }) for APIDoc in cursorAPI]
+    logging.info("Updating documents.")
+    scraperCollection.bulk_write(upserts)
+    logging.info("Data saved succesfully to Mongo.")
+
 
 if __name__ == "__main__":
     main()
